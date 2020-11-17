a532dfe354a56dd73722c72b3bf5793b7f2e47b0
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-11-17 07:29:38 +0700

    Create eloquent models and their relation

---
+++ app/Models/Laporan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Laporan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'laporan';
+
+    public function bukei()
+    {
+        return $this->hasMany(Bukti::class, 'id_laporan');
+    }
+
+    public function cs()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
