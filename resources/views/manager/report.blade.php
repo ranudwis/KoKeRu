6ac2e9a51d6cc71d2d09097e5e2805364af3f371
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-11-18 22:26:59 +0700

    Gabungkan tampilan edit profile dan report ke dashboard

--- resources/views/manager/report.blade.php
+++ resources/views/manager/report.blade.php
@@ -1,35 +1,30 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <form method="get" action="{{ url('manager/laporan') }}">
-            @csrf
+@extends('partial.dashboard_manager')
 
-            <div class="form-group">
-                <label for="date">Tanggal</label>
-                <input id="date" type="date" name="date" />
-            </div>
+@section('content')
+    <form method="get" action="{{ url('manager/laporan') }}">
+        @csrf
 
-            <div class="form-group">
-                <label for="status">Status</label>
+        <div class="form-group">
+            <label for="date">Tanggal</label>
+            <input class="form-control" id="date" type="date" name="date" />
+        </div>
 
-                <select id="status" name="status">
-                    <option value="semua">
-                        Semua
-                    </option>
-                    <option value="belum">
-                        Belum
-                    </option>
-                    <option value="sudah">
-                        Sudah
-                    </option>
-                </select>
-            </div>
+        <div class="form-group">
+            <label for="status">Status</label>
 
-            <button type="submit" class="btn btn-primary mb-2">Tampil</button>
-        </form>
-    </body>
-</html>
+            <select class="form-control" id="status" name="status">
+                <option value="semua">
+                    Semua
+                </option>
+                <option value="belum">
+                    Belum
+                </option>
+                <option value="sudah">
+                    Sudah
+                </option>
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary mb-2">Tampil</button>
+    </form>
+@endsection
