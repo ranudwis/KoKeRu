939da4a5c17787e95be27243f4248f47dc39df75
Dandi Arimansyah <dandimansyah@gmail.com>
2020-12-08 22:36:41 +0700

    Fix tanggal

--- app/Http/Controllers/DashboardCsController.php
+++ app/Http/Controllers/DashboardCsController.php
@@ -6,11 +6,14 @@ use Illuminate\Http\Request;
 use App\Models\Ruang;
 use App\Models\Bukti;
 use App\Models\Laporan;
+use Carbon\Carbon;
 
 class DashboardCsController extends Controller
 {
     public function ruanganCS()
     {
+        $now = new Carbon();
+
         $ruang = auth()->user()
             ->ruang()
             ->withCount([
@@ -20,16 +23,17 @@ class DashboardCsController extends Controller
             ])
             ->get();
 
-        return view('cs.ruang_cs', compact('ruang'));
+        return view('cs.ruang_cs', compact('ruang', 'now'));
     }
 
     public function showCS($id)
     {
+        $now = new Carbon();
         $ruang = Ruang::find($id);
 
         $laporan = $ruang->laporan()->with('bukti')->whereDate('created_at', new \DateTime())->first();
 
-        return view('cs.upload', compact('ruang', 'laporan'));
+        return view('cs.upload', compact('ruang', 'laporan', 'now'));
     }
 
     public function storeCS(Request $request, $id)
