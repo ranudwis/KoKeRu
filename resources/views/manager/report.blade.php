7200eae9a7f9c0cbb53b505c0dacd93f4f4a4c40
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-12-08 19:50:04 +0700

    Revert report.blade.php

--- resources/views/manager/report.blade.php
+++ resources/views/manager/report.blade.php
@@ -1,8 +1,7 @@
 @extends('partial.dashboard_manager')
 
 @section('content')
-
-<form method="get" action="{{ url('manager/laporan') }}">
+    <form method="get" action="{{ url('manager/laporan') }}">
         @csrf
 
         <div class="form-group">
@@ -58,23 +57,6 @@
             </a>
         </p>
 
-        @foreach ($rooms as $room)
-            <tr>
-                <td>
-                    {{ $loop->iteration }}
-                </td>
-                <td>
-                    {{ $room->nama }}
-                </td>
-                <td>
-                    {{ $room->cs->nama }}
-                </td>
-                <td>
-                    {{ $room->laporan_count == 0 ? 'Belum' : 'Sudah' }}
-                </td>
-            </tr>
-        @endforeach
-    </table>   
         @include('partial.reporttable')
     </div>
 @endsection
