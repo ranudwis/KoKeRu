ec1728842a795477dad9b2937f8cdf19951ceda5
Dandi Arimansyah <dandimansyah@gmail.com>
2020-12-09 21:47:31 +0700

    Tambah title home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('home.css') }}">
+    <title>Halaman Utama</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
